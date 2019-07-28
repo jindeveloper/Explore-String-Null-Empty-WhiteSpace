385e48d86eb57ecfb552ee313a41922b45a428e7
Jin Vincent Necesario <39805502+jindeveloper@users.noreply.github.com>
2019-07-28 23:38:58 +0800

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Explore-String-Null-Empty-WhiteSpace")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Towers Watson")]
+[assembly: AssemblyProduct("Explore-String-Null-Empty-WhiteSpace")]
+[assembly: AssemblyCopyright("Copyright © Towers Watson 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8dbb83ff-97f6-409f-913f-149e8a40cc47")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Explore_String_Null_Empty_WhiteSpace
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private string strNull = null;
+        private string strEmptyString = "";
+        private string strWhiteSpace = " ";
+        private string strWhiteSpaces = "  ";
+        private string strWhiteSpaceTab = "\t";
+       
+        [TestMethod]
+        public void Test_String_IsNullOrWhiteSpace()
+        {
+            Assert.AreEqual(strNull, null); 
+            
+            Assert.AreEqual(true, string.IsNullOrWhiteSpace(strEmptyString), $"{strEmptyString} is an empty string");
+
+            Assert.AreEqual(true, string.IsNullOrWhiteSpace(strWhiteSpace));
+
+            Assert.AreEqual(true, string.IsNullOrWhiteSpace(strWhiteSpaces));
+
+            Assert.AreEqual(true, string.IsNullOrWhiteSpace(strWhiteSpaceTab));
+        }
+      
+        [TestMethod]
+        public void Test_String_IsNullOrEmpty()
+        {
+            Assert.AreEqual(strNull, null);
+
+            Assert.AreEqual(true, string.IsNullOrEmpty(strEmptyString), $"{strEmptyString} is an empty string");
+
+            Assert.AreEqual(false, string.IsNullOrEmpty(strWhiteSpace));
+                                           
+            Assert.AreEqual(false, string.IsNullOrEmpty(strWhiteSpaces));
+                                         
+            Assert.AreEqual(false, string.IsNullOrEmpty(strWhiteSpaceTab));
+        }
+    }
+}
+
